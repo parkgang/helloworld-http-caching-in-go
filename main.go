95fc79a3da81e05eec48574e6583e69362671d5b
박경은 <ruddms936@naver.com>
2021-11-15 23:43:02 +0900

    add: 기본 적인 서버 코드

--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"flag"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"log"
+	"net/http"
+	"strconv"
 )
 
+var root = flag.String("root", ".", "file system path")
+
 func main() {
-	fmt.Printf("hello, world\n")
+	http.HandleFunc("/black/", blackHandler)
+	http.Handle("/", http.FileServer(http.Dir(*root)))
+	log.Println("Listening on 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func blackHandler(w http.ResponseWriter, r *http.Request) {
+	m := image.NewRGBA(image.Rect(0, 0, 240, 240))
+	black := color.RGBA{0, 0, 0, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{black}, image.Point{}, draw.Src)
+
+	var img image.Image = m
+	writeImage(w, &img)
+}
+
+// writeImage는 jpeg 형식으로 이미지를 인코딩하고 ResponseWriter에 writes 합니다.
+func writeImage(w http.ResponseWriter, img *image.Image) {
+	buffer := new(bytes.Buffer)
+	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+		log.Println("unable to encode image.")
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		log.Println("unable to write image.")
+	}
 }
