b316070a793c51d398b7b7183055067dcb91e7a9
박경은 <ruddms936@naver.com>
2021-11-16 23:50:58 +0900

    add: 이미지 호출 예제

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var root = flag.String("root", "./assets", "file system path")
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(*root)))
 	http.HandleFunc("/text", textHandler)
-	http.HandleFunc("/black", blackHandler)
+	http.HandleFunc("/image", imageHandler)
 
 	log.Println("Listening on 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -36,7 +36,7 @@ func textHandler(w http.ResponseWriter, r *http.Request) {
 	// 마지막 수정 시간 가져오기
 	file, err := os.Stat(filename)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	modifiedtime := file.ModTime()
 
@@ -47,7 +47,7 @@ func textHandler(w http.ResponseWriter, r *http.Request) {
 	// etag가 변하지 않았다면 304 응답
 	if match := r.Header.Get("If-None-Match"); match != "" {
 		if strings.Contains(match, etag) {
-			fmt.Println("etag가 변하지 않았음으로 304 응답")
+			log.Println("etag가 변하지 않았음으로 304 응답")
 			w.WriteHeader(http.StatusNotModified)
 			return
 		}
@@ -56,43 +56,26 @@ func textHandler(w http.ResponseWriter, r *http.Request) {
 	// 파일을 읽은 후 응답
 	dat, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	fmt.Println("파일 응답")
+	log.Println("파일 응답")
 	fmt.Fprint(w, string(dat))
 }
 
-func blackHandler(w http.ResponseWriter, r *http.Request) {
-	// 캐시 로직
-	key := "black"
-	e := `"` + key + `"`
-	w.Header().Set("Etag", e)
-	w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
-
-	if match := r.Header.Get("If-None-Match"); match != "" {
-		if strings.Contains(match, e) {
-			w.WriteHeader(http.StatusNotModified)
-			return
-		}
-	}
-
-	fmt.Println("캐시되지 않아서 요청이 꽂이고 있습니다.")
-	// 검은색 이미지 생성
+func imageHandler(w http.ResponseWriter, r *http.Request) {
 	m := image.NewRGBA(image.Rect(0, 0, 240, 240))
-	black := color.RGBA{0, 0, 0, 255}
-	draw.Draw(m, m.Bounds(), &image.Uniform{black}, image.Point{}, draw.Src)
+	// 검은색 이미지 생성
+	tempImage := color.RGBA{0, 0, 0, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{tempImage}, image.Point{}, draw.Src)
 
+	// jpeg 형식으로 이미지를 인코딩하고 ResponseWriter에 writes 합니다.
 	var img image.Image = m
-	writeImage(w, &img)
-}
-
-// writeImage는 jpeg 형식으로 이미지를 인코딩하고 ResponseWriter에 writes 합니다.
-func writeImage(w http.ResponseWriter, img *image.Image) {
 	buffer := new(bytes.Buffer)
-	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+	if err := jpeg.Encode(buffer, img, nil); err != nil {
 		log.Println("unable to encode image.")
 	}
 
+	w.Header().Set("Cache-Control", "max-age=5")
 	w.Header().Set("Content-Type", "image/jpeg")
 	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
 	if _, err := w.Write(buffer.Bytes()); err != nil {
